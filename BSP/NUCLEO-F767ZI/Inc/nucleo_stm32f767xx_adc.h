7afdebd5f187191a690623e79fcd2953effa4a27
Fahad Ali Sarwar <32795677+fahadalisarwar1@users.noreply.github.com>
2021-11-09 16:00:00 +0100

    Update nucleo_stm32f767xx_adc.h

--- BSP/NUCLEO-F767ZI/Inc/nucleo_stm32f767xx_adc.h
+++ BSP/NUCLEO-F767ZI/Inc/nucleo_stm32f767xx_adc.h
@@ -8,6 +8,12 @@
 #ifndef NUCLEO_F767ZI_INC_NUCLEO_STM32F767XX_ADC_H_
 #define NUCLEO_F767ZI_INC_NUCLEO_STM32F767XX_ADC_H_
 
+#include "main.h"
+
+
+void adc_GPIO_init(void);
+
+bool adc_single_conversion_init(void);
 
 
 #endif /* NUCLEO_F767ZI_INC_NUCLEO_STM32F767XX_ADC_H_ */
